a33903e7a291a78ab476542d0cba1009ad530ea6
aryaminus <sunim.54@gmail.com>
2022-09-09 21:27:35 +0545

    FIX #5 :hammer: Handle character and range

--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,50 @@
-import tokenizer from "./tokenizer";
+import tokenizer, { openBracketTokenizer } from "./tokenizer";
 
-export function parser(expression: ReturnType<typeof tokenizer>) {
-	return expression;
+import { getRandomValueBetween } from "./utils";
+
+function buildString(
+	token: ReturnType<typeof openBracketTokenizer>,
+	index: number
+) {
+	const currentValueList = token.valueList[index];
+	let generatedCharacter = "";
+
+	if (currentValueList.type === "CHARACTER") {
+		generatedCharacter += currentValueList.value;
+	}
+
+	if (currentValueList.type === "RANGE") {
+		generatedCharacter += String.fromCharCode(
+			getRandomValueBetween(
+				currentValueList.from.value.charCodeAt(0),
+				currentValueList.to.value.charCodeAt(0)
+			)
+		);
+	}
+
+	return generatedCharacter;
+}
+
+function generator(token: ReturnType<typeof openBracketTokenizer>) {
+	let generatedString = "";
+
+	const repeat = getRandomValueBetween(
+		token.startingRange,
+		token.endingRange
+	);
+
+	for (let i = 0; i < repeat; i++) {
+		const index = getRandomValueBetween(0, token.valueList.length - 1);
+		generatedString += buildString(token, index);
+	}
+
+	return generatedString;
+}
+
+export function parser(tokens: ReturnType<typeof tokenizer>) {
+	for (let token of tokens) {
+		return generator(token);
+	}
 }
 
 export default parser;
