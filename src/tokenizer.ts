3fbdb05f366aa7537d04d56950036ec84ddf9714
aryaminus <sunim.54@gmail.com>
2022-09-09 16:59:30 +0545

    FIX #4 :hammer: Create tokenizer for bracket

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,18 +1,59 @@
-function getCharactersWithBracket(expression: string, originalIndex: number) {
-	const closingIndex = expression.indexOf("]");
-	const exactExpression = expression.slice(originalIndex, closingIndex + 1);
+function constantTokenizer(expression: string, originalIndex: number) {}
 
-	return { openingIndex: originalIndex, closingIndex, exactExpression };
+function characterTokenizer(charactersList: RegExpMatchArray) {
+	return charactersList.map((c) => {
+		if (c.length == 1) {
+			return {
+				type: "CHARACTER",
+				value: c,
+			};
+		} else {
+			return {
+				type: "RANGE",
+				from: {
+					type: "CHARACTER",
+					value: c[0],
+				},
+				to: {
+					type: "CHARACTER",
+					value: c[c.length],
+				},
+			};
+		}
+	});
 }
 
-function openBracketTokenizer(expression: string, originalIndex: number) {
-	const charactersWithBracket = getCharactersWithBracket(
-		expression,
-		originalIndex
+function openBracketTokenizer(
+	expression: string,
+	indexOfOpeningBracket: number
+) {
+	let isElm = false;
+
+	// Get characters inside bracket and create matched array
+	let indexOfClosingBracket = expression.indexOf("]");
+	let charactersList = expression
+		.slice(indexOfOpeningBracket + 1, indexOfClosingBracket)
+		.match(/(.-.|.)/g);
+
+	// Check for Elm, mark it and remove from charactersList
+	if (
+		charactersList.length &&
+		charactersList[0] === "^" &&
+		expression[1] !== "\\"
+	) {
+		isElm = true;
+		charactersList = charactersList.slice(1, charactersList.length);
+	}
+
+	// Build token for values
+	const tokens = characterTokenizer(charactersList);
+
+	// Find Quantifier Range
+	const stringAfterClosingBracket = expression.slice(
+		indexOfClosingBracket + 1,
+		expression.length
 	);
 }
-
-function constantTokenizer(expression: string, originalIndex: number) {}
 
 export function tokenizer(expression: string) {
 	let tokenList = [];
