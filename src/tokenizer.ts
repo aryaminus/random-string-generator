1f3e529c5acc0d8d6f985b5ba684637fc8c9e903
aryaminus <sunim.54@gmail.com>
2022-09-09 14:54:03 +0545

    FIX #4 :hammer: Create structure for tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,26 @@
-export function tokenizer(str: string) {
- return [];
+function bracketTokenizer(expression: string, originalIndex: number) {}
+
+function constantTokenizer(expression: string, originalIndex: number) {}
+
+export function tokenizer(expression: string) {
+	let tokenList = [];
+	let i = 0;
+
+	while (i < expression.length) {
+		let tokenValue;
+
+		if (expression[i] === "[") {
+			tokenValue = bracketTokenizer(expression, i);
+		} else {
+			tokenValue = constantTokenizer(expression, i);
+		}
+
+		tokenList.push(tokenValue.tokens);
+
+		i = tokenValue.newIndex + 1;
+	}
+
+	return tokenList;
 }
 
 export default tokenizer;
