4d735a8b0e21a3f17288111c40d9342aac3a8f28
aryaminus <sunim.54@gmail.com>
2022-09-09 17:08:36 +0545

    FIX #4 :hammer: Introduce Quantifier function

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,95 @@
 function constantTokenizer(expression: string, originalIndex: number) {}
 
+function findQuantifierRange(afterTokenExpression: string) {
+	const nextCharacters = ["?", "*", "+", "{"];
+
+	if (
+		afterTokenExpression.length === 0 ||
+		!nextCharacters.includes(afterTokenExpression[0])
+	) {
+		return { startingRange: 1, endingRange: 1, operationLength: 0 };
+	}
+
+	let startingRange = 1;
+	let endingRange = 1;
+	let operationLength = 1; // length of string used in quantifier
+
+	let character = afterTokenExpression[0];
+	switch (character) {
+		case "?": {
+			startingRange = 0;
+			break;
+		}
+		case "*": {
+			startingRange = 0;
+			endingRange = Infinity;
+			break;
+		}
+		case "+": {
+			startingRange = 1;
+			endingRange = Infinity;
+			break;
+		}
+		case "{": {
+			let closingParenthesisIndex = afterTokenExpression.indexOf("]");
+
+			// If } does not exist set operation length as 0 and break the case
+			if (closingParenthesisIndex === -1) {
+				operationLength = 0;
+				break;
+			}
+
+			const stringInsideParenthesis = afterTokenExpression.slice(
+				1,
+				closingParenthesisIndex
+			);
+			operationLength = stringInsideParenthesis.length + 2;
+
+			let range = stringInsideParenthesis.split(",");
+
+			// If range is higher than 2 values set operation length as 0 and break the case
+			if (range.length > 2) {
+				operationLength = 0;
+				break;
+			}
+
+			// If first item is not empty
+			if (range[0] !== "") {
+				let first = parseInt(range[0]);
+
+				// If first value is Not a Number set operation length as 0 and break the case
+				if (first === NaN) {
+					operationLength = 0;
+					break;
+				}
+
+				startingRange = first;
+				endingRange = first;
+			}
+
+			if (range.length === 2) {
+				// If the second item is empty set endingRange as Inf. and break the case
+				if (range[1] === "") {
+					endingRange = Infinity;
+					break;
+				}
+
+				let second = parseInt(range[1]);
+
+				if (second === NaN) {
+					operationLength = 0;
+					startingRange = 1;
+				} else {
+					endingRange = second;
+				}
+			}
+			break;
+		}
+	}
+
+	return { startingRange, endingRange, operationLength };
+}
+
 function characterTokenizer(charactersList: RegExpMatchArray) {
 	return charactersList.map((c) => {
 		if (c.length == 1) {
@@ -53,24 +143,33 @@ function openBracketTokenizer(
 		indexOfClosingBracket + 1,
 		expression.length
 	);
+	let range = findQuantifierRange(stringAfterClosingBracket); // find quantifier
+
+	return {
+		type: "BRACKET",
+		tokens,
+		isElm,
+		closingIndex: indexOfClosingBracket,
+		...range,
+	};
 }
 
 export function tokenizer(expression: string) {
 	let tokenList = [];
-	let i = 0;
+	let openingIndex = 0;
 
-	while (i < expression.length) {
-		let tokenValue;
+	while (openingIndex < expression.length) {
+		let tokenValue: ReturnType<typeof openBracketTokenizer>;
 
-		if (expression[i] === "[") {
-			tokenValue = openBracketTokenizer(expression, i);
+		if (expression[openingIndex] === "[") {
+			tokenValue = openBracketTokenizer(expression, openingIndex);
 		} else {
-			tokenValue = constantTokenizer(expression, i);
+			// tokenValue = constantTokenizer(expression, openingIndex);
 		}
 
 		tokenList.push(tokenValue.tokens);
 
-		i = tokenValue.newIndex + 1;
+		openingIndex = tokenValue.closingIndex + tokenValue.operationLength + 1;
 	}
 
 	return tokenList;
