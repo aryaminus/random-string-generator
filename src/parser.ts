8d787ccf0ece7e7a4f73849c9276e80ab991e4f2
aryaminus <sunim.54@gmail.com>
2022-09-09 23:04:50 +0545

    FIX #5 :hammer: Handle elements in parser

--- src/parser.ts
+++ src/parser.ts
@@ -2,6 +2,38 @@ import tokenizer, { openBracketTokenizer } from "./tokenizer";
 
 import { getRandomValueBetween } from "./utils";
 
+function getElements(token: ReturnType<typeof openBracketTokenizer>) {
+	let map = {};
+	let includedElements: number[] = [];
+
+	if (!token.isElement) {
+		return includedElements;
+	}
+
+	for (let item of token.valueList) {
+		switch (item.type) {
+			case "CHARACTER":
+				map[item.value.charCodeAt(0)] = 1;
+			case "RANGE":
+				for (
+					let i = item.from.value.charCodeAt(0);
+					i <= item.to.value.charCodeAt(0);
+					i++
+				) {
+					map[i] = 1;
+				}
+		}
+	}
+
+	for (let i = 32; i <= 126; i++) {
+		if (map[i] === undefined) {
+			includedElements.push(i);
+		}
+	}
+
+	return includedElements;
+}
+
 function buildString(
 	token: ReturnType<typeof openBracketTokenizer>,
 	index: number
@@ -25,26 +57,43 @@ function buildString(
 	return generatedCharacter;
 }
 
-function generator(token: ReturnType<typeof openBracketTokenizer>) {
-	let generatedString = "";
-
+function generator(
+	generatedString: string,
+	token: ReturnType<typeof openBracketTokenizer>
+) {
 	const repeat = getRandomValueBetween(
 		token.startingRange,
 		token.endingRange
 	);
 
+	let includedElements = getElements(token);
+
 	for (let i = 0; i < repeat; i++) {
-		const index = getRandomValueBetween(0, token.valueList.length - 1);
-		generatedString += buildString(token, index);
+		if (token.isElement) {
+			generatedString += String.fromCharCode(
+				includedElements[
+					getRandomValueBetween(0, includedElements.length - 1)
+				]
+			);
+		} else {
+			generatedString += buildString(
+				token,
+				getRandomValueBetween(0, token.valueList.length - 1)
+			);
+		}
 	}
 
 	return generatedString;
 }
 
 export function parser(tokens: ReturnType<typeof tokenizer>) {
+	let generatedString = "";
+
 	for (let token of tokens) {
-		return generator(token);
+		generatedString = generator(generatedString, token);
 	}
+
+	return generatedString;
 }
 
 export default parser;
